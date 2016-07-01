33eb88cb69d870b898031d150aeec20a26470086
Sean Carey <sean.carey@technekes.com>
2016-06-30 23:11:32 -0400

    Don't eager load application

--- lib/fencepost/acts_as_fencepost.rb
+++ lib/fencepost/acts_as_fencepost.rb
@@ -1,10 +1,8 @@
 module Fencepost
   module ActsAsFencepost
-    extend ActiveSupport::Concern
     def fencepost
       Fencepost.new(params)
     end
   end
 end
 
-ActionController::Base.send :include, Fencepost::ActsAsFencepost
--- lib/fencepost/railtie.rb
+++ lib/fencepost/railtie.rb
@@ -2,9 +2,13 @@ require 'yaml'
 module Fencepost
   class Railtie < ::Rails::Railtie
     config.after_initialize do
-      Rails.application.eager_load!
-      file_name = "#{Rails.root}/config/fencepost.yml"
-      Fencepost.model_list = YAML.load_file(file_name) if File.exists?(file_name)
+      ActiveSupport.on_load :action_controller do
+        file_name = "#{Rails.root}/config/fencepost.yml"
+        if File.exists?(file_name)
+          ::Fencpost::Fencpost.model_list = YAML.load_file(file_name)
+        end
+        include ::Fencepost::ActsAsFencepost
+      end
     end
   end
 end
